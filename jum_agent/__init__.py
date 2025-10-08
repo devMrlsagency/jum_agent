1f5c8d13836fac2826931734cd5003a2cc7fac88
devMrlsagency <122008760+devMrlsagency@users.noreply.github.com>
2025-10-08 09:42:21 -0700

    Create __init__.py

---
+++ jum_agent/__init__.py
@@ -0,0 +1,8 @@
+"""Jum Agent package.
+
+This package implements a simple multi agent framework for automating
+development tasks using local large language models.  See the README for
+more information.
+"""
+
+from .orchestrator import Orchestrator  # noqa: F401
